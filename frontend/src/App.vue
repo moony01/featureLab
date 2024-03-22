ad1287e4ced0a3bf762ea96b62d3dc8a636ce991
mhhan <mun01180@gmail.com>
2024-03-22 17:32:04 +0900

    같은 버전의 부트스트랩을 설치했는데 결과가 다름....

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <!-- <HanMoonHee msg="hello hanmoonhee"/> -->
+  <div
+    v-show="this.$store.state.layout === 'default'"
+    class="landing-bg bg-primary position-fixed w-100"
+  >ㅎㅇ</div>
+  <button v-on:click="changeLayoutToLanding">TEST</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import HanMoonHee from './components/hanmoonhee/HanMoonHee.vue'
+import { mapMutations } from "vuex";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    // HanMoonHee
+  },
+  methods: {
+    ...mapMutations([
+      'setLayout' // 'setLayout' 뮤테이션을 메소드로 매핑
+    ]),
+    changeLayoutToLanding() {
+      this.setLayout('landing'); // 'setLayout' 뮤테이션 호출, 'landing'을 인자로 전달
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
+// main.js 또는 app.js
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).mount('#app')
+
+const appInstance = createApp(App);
+
+appInstance.use(store);
+appInstance.mount("#app");
