cc66a0475f20baa45e4a47a530593ee94152fcd6
mhhan <mun01180@gmail.com>
2024-04-05 23:08:35 +0900

    quasar 설치 완료

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,11 @@
-<template>
+<!-- <template>
   <div>
     <NuxtWelcome />
   </div>
+</template> -->
+
+<template>
+  <q-btn color="primary" label="Primary" />
+  <QBtn color="secondary" label="Secondary" />
+  <LazyQBtn color="amber" glossy label="Amber" />
 </template>
