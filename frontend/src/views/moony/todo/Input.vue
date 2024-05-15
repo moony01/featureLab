e0a400c0af6afc1c0daad88bc15ec67d726d6eb6
mhhan <mun01180@gmail.com>
2024-05-15 20:59:04 +0900

    todo input 스타일링

--- frontend/src/views/moony/todo/Input.vue
+++ frontend/src/views/moony/todo/Input.vue
@@ -1,30 +1,61 @@
 <template>
-    <div>
-      <!-- 
+  <div class="inputBox shadow">
+    <!-- 
         input에 입력된 텍스트 값을 동적으로 뷰 인스턴스에 매핑
        -->
-      <input type="text" v-model="newTodoItem">
-      <button v-on:click="addTodo">add</button>
-    </div>
-  </template>
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
+</template>
   
   <script>
-  export default {
-    data: function() {
-      return {
-        newTodoItem: ""
-      }
+export default {
+  data: function () {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo: function () {
+      // 저장하는 로직
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
     },
-    methods: {
-      addTodo: function() {
-        console.log(this.newTodoItem);
-        // 저장하는 로직
-        localStorage.setItem(this.newTodoItem, this.newTodoItem);
-        this.newTodoItem = '';
-      }
-    }
-  };
-  </script>
+    clearInput: function () {
+      this.newTodoItem = "";
+    },
+  },
+};
+</script>
   
-  <style lang="scss" scoped>
-  </style>
+<style lang="scss" scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+.closeModalBtn {
+  color: #42b983;
+}
+</style>
